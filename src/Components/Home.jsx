3ca38fd7e50177d58ed7a09962a4526cbbbc5586
Arif-khan9 <ak4040651@gmail.com>
2025-08-16 13:30:25 +0530

    transition add

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,7 +5,6 @@ import { FaBookOpen } from "react-icons/fa";
 const Home = () => {
 
   const [data, setData] = useState([]);
-  // const [searchTerm, setSearchTerm] = useState(""); 
 
   const API = `https://www.themealdb.com/api/json/v1/1/categories.php`;
 
@@ -24,9 +23,6 @@ const Home = () => {
     fetchBoxData();
   }, []);
 
-  // const filteredData = data.filter(item =>
-  //   item.strCategory.toLowerCase().includes(searchTerm.toLowerCase())
-  // );
 
   return (
     <>
@@ -38,10 +34,10 @@ const Home = () => {
           Discover delicious meals that will tantalize your taste buds and <br /> brighten your day!
         </p>
         <div className='md:flex md:flex-row flex flex-col gap-2.5'>
-          <button className='py-3 px-6 rounded-full bg-amber-500 hover:bg-amber-700 hover:shadow-lg hover:translate-y-1 text-white flex items-center gap-2.5'>
+          <button className='py-3 px-6 rounded-full bg-amber-500 hover:bg-amber-700 hover:shadow-lg hover:-translate-y-1 transition-transform duration-300 text-white flex items-center gap-2.5'>
             <FaBookOpen /> Today's Special
           </button>
-          <button className='py-3 px-6 rounded-full hover:bg-gray-50 text-amber-800 border border-amber-300 hover:translate-y-1 flex items-center gap-2.5'>
+          <button className='py-3 px-6 rounded-full hover:bg-gray-50 text-amber-800 border border-amber-300 hover:-translate-y-1 transition-transform duration-300 flex items-center gap-2.5'>
             <IoMdSearch /> Search a Recipe
           </button>
         </div>
@@ -50,9 +46,9 @@ const Home = () => {
         <div className="flex flex-col justify-center text-center items-center gap-6 pt-20">
           <h3 className='text-3xl md:text-4xl text-amber-900 font-bold'>Browse by Category</h3>
           <div className='flex items-center relative gap-2.5'>
-            <IoMdSearch className='absolute left-3 text-amber-900 text-xl' />
+            <IoMdSearch className='absolute hover:translate-y-1  left-3 text-amber-900 text-xl' />
             <input
-              className='border border-amber-300 rounded-full p-3 px-9 placeholder:text-amber-600'
+              className='border bg-white border-amber-300 rounded-full hover:-translate-y-1 transition-transform duration-300  p-3 px-9 placeholder:text-amber-600'
               type="text"
               placeholder='Search for Category'
              
@@ -60,26 +56,31 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="flex flex-col md:grid md:grid-cols-2  lg:grid-cols-3 place-items-center md:px-10  py-16">
+        <div className="flex flex-col  md:grid md:grid-cols-2  gap-10 lg:grid-cols-3 place-items-center md:px-10  py-16">
           {
-            data.map((item, index) => (
-              <div
+            data.map((item, index) => {
+              return(
+                <div
                 key={index}
-                className="bg-white h-90 w-80 mt-10 px-5 hover:shadow-2xl flex flex-col justify-center rounded-2xl"
+                className=" group  bg-white w-80 pb-15 hover:-translate-y-1 transition-transform duration-300 gap-4 px-5 hover:shadow-2xl flex flex-col justify-center rounded-2xl"
               >
                 <img
                   src={item.strCategoryThumb}
                   alt={item.strCategory}
-                  className="rounded-xl h-40 object-cover"
+                  className="rounded-xl h-40 w-full object-contain transition-transform  duration-300 group-hover:scale-110"
                 />
                 <h3 className="text-xl text-amber-900 hover:text-amber-700 font-bold mt-4">
                   {item.strCategory}
                 </h3>
                 <p className="text-amber-800 text-sm mt-2">
-                  {item.strCategoryDescription.slice(0, 80)}...
+                  {item.strCategoryDescription.slice(0, 120)}...
                 </p>
+                
               </div>
-            ))
+              )
+            }
+              
+            )
           }
           
         </div>
